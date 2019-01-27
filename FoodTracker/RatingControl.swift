4b8743f08e934fd67368ff176e50184ed812a953
Manij <manij07@hotmail.co.uk>
2019-01-27 11:46:58 +0000

    Added a class from rating control stack view

---
+++ FoodTracker/RatingControl.swift
@@ -0,0 +1,23 @@
+//
+//  RatingControl.swift
+//  FoodTracker
+//
+//  Created by Manij Basnet on 27/01/2019.
+//  Copyright © 2019 Basnet Inc. All rights reserved.
+//
+
+import UIKit
+
+class RatingControl: UIStackView {
+
+    //MARK: Initialization
+    
+    override init(frame: CGRect) {
+        super.init(frame: frame)
+    }
+    
+    required init(coder: NSCoder) {
+        super.init(coder: coder)
+    }
+
+}

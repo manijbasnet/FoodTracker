32074df7f8ad1d04bf0e03a71f3724a8fe8a4f1d
Manij <manij07@hotmail.co.uk>
2019-01-27 14:33:27 +0000

    added unit testing for Meal class

---
+++ FoodTrackerTests/FoodTrackerTests.swift
@@ -0,0 +1,53 @@
+//
+//  FoodTrackerTests.swift
+//  FoodTrackerTests
+//
+//  Created by Manij Basnet on 27/01/2019.
+//  Copyright © 2019 Basnet Inc. All rights reserved.
+//
+
+import XCTest
+@testable import FoodTracker
+
+class FoodTrackerTests: XCTestCase {
+
+    override func setUp() {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+    
+    func testMealInitializationSucceeds(){
+        let zeroRatingMeal = Meal.init(name: "Zero", photo: nil, rating: 0)
+        XCTAssertNotNil(zeroRatingMeal)
+        
+        let postiveRatingMeal = Meal.init(name: "Positive", photo: nil, rating: 5)
+        XCTAssertNotNil(postiveRatingMeal)
+    }
+    
+    func testMealInitializationFails(){
+        let negativeRatingMeal = Meal.init(name: "Negative", photo: nil, rating: -1)
+        XCTAssertNil(negativeRatingMeal)
+        
+        let largeRatingMeal = Meal.init(name: "Large", photo: nil, rating: 6)
+        XCTAssertNil(largeRatingMeal)
+        
+        let emptyStringMeal = Meal.init(name: "", photo: nil, rating: 0)
+        XCTAssertNil(emptyStringMeal)
+    }
+
+}
